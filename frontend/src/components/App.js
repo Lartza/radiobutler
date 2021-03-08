2af827e23e2954b5a695627be3a57bcbc684e242
Lartza <Lartza@users.noreply.github.com>
2021-03-08 20:13:49 +0200

    Create services with React

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,14 @@
 import React, {Component} from "react";
 import {render} from "react-dom";
+import Cookies from "universal-cookie/es6";
 
 // React form
 class MyForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      shortname: "",
-      mediumname: "",
+      short_name: "",
+      medium_name: "",
       short_description: "",
       link: "",
       logo: "",
@@ -19,98 +20,74 @@ class MyForm extends React.Component {
     let value = event.target.value;
     this.setState({ [name]: value });
   };
+
+  mySubmitHandler = (event) => {
+      event.preventDefault();
+      const form = event.target;
+      const data = new FormData(form);
+
+      const cookies = new Cookies();
+
+      fetch("/api/services/", {
+        method: "POST",
+        headers: {
+          'X-CSRFToken': cookies.get('csrftoken')
+        },
+        body: data,
+
+      }).then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+      });
+  };
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.mySubmitHandler}>
         <h1>Edit station info - REACT</h1>
         <h2>Name</h2>
-        <label for="shortname">Short name (8 chars) </label>
-        <br></br>
-        <input type="text" name="shortname" onChange={this.myChangeHandler} />
-        <br></br>
+        <label htmlFor="shortname">Short name (8 chars) </label>
+        <br/>
+        <input type="text" name="short_name" onChange={this.myChangeHandler} />
+        <br/>
 
-        <label for="mediumname">Medium name (16 chars) </label>
-        <br></br>
-        <input type="text" name="mediumname" onChange={this.myChangeHandler} />
-        <br></br>
+        <label htmlFor="mediumname">Medium name (16 chars) </label>
+        <br/>
+        <input type="text" id="mediumnane" name="medium_name" onChange={this.myChangeHandler} />
+        <br/>
         <h2>Description</h2>
-        <label for="short_description">Short description (180 chars)</label>
-        <br></br>
-        <textarea
-          id="desc"
-          name="short_description"
-          onChange={this.myChangeHandler}
-        ></textarea>
-        <br></br>
+        <label htmlFor="desc">Short description (180 chars)</label>
+        <br/>
+        <textarea id="desc" name="short_description" onChange={this.myChangeHandler} />
+        <br/>
 
         <h2>Logo</h2>
-        <label for="logo">Image type: jpeg, image size: 600 x 600 px</label>
-        <br></br>
+        <label htmlFor="logo">Image type: jpeg, image size: 600 x 600 px</label>
+        <br/>
         <input type="file" name="logo" onChange={this.myChangeHandler} />
-        <br></br>
+        <br/>
 
         <h2>Bearers</h2>
         <h2>RadioDNS Parameters</h2>
-        <label for="fqdn">FQDN</label>
-        <br></br>
+        <label htmlFor="fqdn">FQDN</label>
+        <br/>
         <input type="text" name="fqdn" onChange={this.myChangeHandler} />
-        <br></br>
+        <br/>
 
-        <label for="fqdn">Service Identifier</label>
-        <br></br>
+        <label htmlFor="fqdn">Service Identifier</label>
+        <br/>
         <input
           type="text"
           name="service_identifier"
           onChange={this.myChangeHandler}
         />
-        <br></br>
+        <br/>
 
         <input type="submit" value="SAVE" />
       </form>
     );
   }
 }
-
-/*
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: [],
-            loaded: false,
-            placeholder: "Loading"
-        };
-    }
-
-    componentDidMount() {
-        fetch("api/helloworld")
-            .then(response => {
-                if (response.status > 400) {
-                    return this.setState(() => {
-                        return {placeholder: "Something went wrong!"};
-                    });
-                }
-                return response.json();
-            })
-            .then(data => {
-                this.setState(() => {
-                    return {
-                        data,
-                        loaded: true
-                    };
-                });
-            });
-    }
-
-    render() {
-        return (
-            <span>
-                {this.state.data.toString()}
-            </span>
-        );
-    }
-}
-*/
 
 export default MyForm;
 
