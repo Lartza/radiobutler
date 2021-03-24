6600bec16f5e48d275a638ee132658958072fd08
Lartza <Lartza@users.noreply.github.com>
2021-03-24 20:43:59 +0200

    git commit in footer

--- frontend/views.py
+++ frontend/views.py
@@ -1,16 +1,24 @@
+import subprocess
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 
+def get_commit():
+    return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).strip().decode()
+
+
 def index(request):
-    return render(request, 'frontend/index.html')
+    context = {'commit': get_commit()}
+    return render(request, 'frontend/index.html', context)
 
 
 @login_required
 def service(request):
-    return render(request, 'frontend/service.html')
+    context = {'commit': get_commit()}
+    return render(request, 'frontend/service.html', context)
 
 
 @login_required
 def slideshow(request):
-    return render(request, 'frontend/slideshow.html')
+    context = {'commit': get_commit()}
+    return render(request, 'frontend/slideshow.html', context)
