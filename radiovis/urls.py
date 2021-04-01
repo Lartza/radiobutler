7372c40a254de82735ebdc356445540798f89e1f
Lartza <Lartza@users.noreply.github.com>
2021-04-01 13:21:22 +0300

    Paginated image api, fix slideshow sending

--- radiovis/urls.py
+++ radiovis/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 router = routers.SimpleRouter()
 router.register(r'imageslides', views.ImageSlideViewSet)
-router.register(r'textslides', views.TextSlideViewSet, basename='textslides')
+router.register(r'textslides', views.TextSlideViewSet)
+router.register(r'images', views.ImageViewSet)
--- radiovis/views.py
+++ radiovis/views.py
@@ -3,14 +3,71 @@ import stomp
 
 from rest_framework import viewsets
 from rest_framework import permissions
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
 from stomp.exception import ConnectFailedException
+from datetime import datetime, timezone
 
 from radiodns.settings import STOMP_HOST, STOMP_PORT, STOMP_USERNAME, STOMP_PASSWORD
 
-from .models import ImageSlide, TextSlide
-from .serializers import ImageSlideSerializer, TextSlideSerializer
+from .models import ImageSlide, TextSlide, Image
+from .serializers import ImageSlideSerializer, TextSlideSerializer, ImageSerializer
+
+
+def send_stomp_image(instance, url):
+    headers = {}
+
+    if instance.trigger_time:
+        headers['trigger_time'] = instance.trigger_time.isoformat()
+    else:
+        headers['trigger_time'] = 'NOW'
+
+    if instance.link:
+        headers['link'] = instance.link
+
+    try:
+        # Reduce blocking by reconnecting only once
+        conn = stomp.Connection([(STOMP_HOST, STOMP_PORT)], reconnect_attempts_max=1)
+        # Create listener to catch error messages from Stomp
+        lst = ErrorListener()
+        conn.set_listener('', lst)
+
+        conn.connect(STOMP_USERNAME, STOMP_PASSWORD, wait=True)
+        conn.send(body=f'SHOW {url}',
+                  headers=headers,
+                  destination='/topic/fm/6e1/6024/09840/image')
+        conn.disconnect()
+        time.sleep(2)
+
+        # Check for received error messages
+        if lst.error:
+            raise stomp.exception.StompException(f'{lst.error}')
+        instance.sent = datetime.now(timezone.utc)
+        instance.save()
+    except (ConnectFailedException, stomp.exception.StompException):
+        raise
+
+
+def send_stomp_text(instance):
+    try:
+        # Reduce blocking by reconnecting only once
+        conn = stomp.Connection([(STOMP_HOST, STOMP_PORT)], reconnect_attempts_max=1)
+        # Create listener to catch error messages from Stomp
+        lst = ErrorListener()
+        conn.set_listener('', lst)
+
+        conn.connect(STOMP_USERNAME, STOMP_PASSWORD, wait=True)
+        conn.send(body=f'TEXT {instance.message}',
+                  destination='/topic/fm/6e1/6024/09840/text')
+        conn.disconnect()
+        time.sleep(2)
+
+        # Check for received error messages
+        if lst.error:
+            raise stomp.exception.StompException(f'{lst.error}')
+        instance.sent = datetime.now(timezone.utc)
+        instance.save()
+    except ConnectFailedException:
+        raise
 
 
 class ErrorListener(stomp.ConnectionListener):
@@ -29,43 +86,9 @@ class ImageSlideViewSet(viewsets.ModelViewSet):
     serializer_class = ImageSlideSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-    def create(self, request, *args, **kwargs):
-        """Sends SHOW messages to Stomp server."""
-        headers = {}
-        slide = self.get_object()
-
-        if slide.trigger_time:
-            headers['trigger_time'] = slide.trigger_time.isoformat()
-        else:
-            headers['trigger_time'] = 'NOW'
-
-        if slide.link:
-            headers['link'] = slide.link
-
-        try:
-            # Reduce blocking by reconnecting only once
-            conn = stomp.Connection([(STOMP_HOST, STOMP_PORT)], reconnect_attempts_max=1)
-            # Create listener to catch error messages from Stomp
-            lst = ErrorListener()
-            conn.set_listener('', lst)
-
-            conn.connect(STOMP_USERNAME, STOMP_PASSWORD, wait=True)
-            conn.send(body=f'SHOW {request.scheme}://{request.META.get("HTTP_HOST")}{slide.image.url}',
-                      headers=headers,
-                      destination='/topic/fm/6e1/6024/09840/image')
-            conn.disconnect()
-            time.sleep(2)
-
-            # Check for received error messages
-            if lst.error:
-                return Response(f'{lst.error}', status=status.HTTP_502_BAD_GATEWAY)
-            slide.sent = True
-            slide.save()
-
-            return super().create(request, *args, **kwargs)
-        except ConnectFailedException:
-            return Response(f'Connection to stomp server {STOMP_HOST}:{STOMP_PORT} failed',
-                            status=status.HTTP_502_BAD_GATEWAY)
+    def perform_create(self, serializer):
+        instance = serializer.save()
+        send_stomp_image(instance, self.request.build_absolute_uri(instance.image.image.url))
 
 
 class TextSlideViewSet(viewsets.ModelViewSet):
@@ -73,27 +96,13 @@ class TextSlideViewSet(viewsets.ModelViewSet):
     serializer_class = TextSlideSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-    def create(self, request, *args, **kwargs):
-        """Send TEXT messages to Stomp server without creating any objects."""
-        text = request.data['text']
+    def perform_create(self, serializer):
+        instance = serializer.save()
+        send_stomp_text(instance)
 
-        try:
-            # Reduce blocking by reconnecting only once
-            conn = stomp.Connection([(STOMP_HOST, STOMP_PORT)], reconnect_attempts_max=1)
-            # Create listener to catch error messages from Stomp
-            lst = ErrorListener()
-            conn.set_listener('', lst)
 
-            conn.connect(STOMP_USERNAME, STOMP_PASSWORD, wait=True)
-            conn.send(body=f'TEXT {text}',
-                      destination='/topic/fm/6e1/6024/09840/text')
-            conn.disconnect()
-            time.sleep(2)
-
-            # Check for received error messages
-            if lst.error:
-                return Response(f'{lst.error}', status=status.HTTP_502_BAD_GATEWAY)
-            return super().create(request, *args, **kwargs)
-        except ConnectFailedException:
-            return Response(f'Connection to stomp server {STOMP_HOST}:{STOMP_PORT} failed',
-                            status=status.HTTP_502_BAD_GATEWAY)
+class ImageViewSet(viewsets.ModelViewSet):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination_class = PageNumberPagination
